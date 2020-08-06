0b096acce0634636863a6fc5631fdb84abe8db0b
kran4 <cuiusvis.hominis.est.errare@gmail.com>
2020-08-05 21:52:16 -0400

    2 files modified

--- app.js
+++ app.js
@@ -14,3 +14,26 @@ angular.module('app').controller('MainCtrl', function ($scope){
 
 // Bootstrap angular onto the 'app' element, injecting the 'app' module.
 angular.bootstrap(document.getElementById('app'), ['app']);
+
+  var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+  function doCORSRequest(options, fcn) {
+    var x = new XMLHttpRequest();
+    x.open(options.method, cors_api_url + options.url);
+    x.onload = x.onerror = function() {
+      fcn(x);
+    };
+    if (/^POST/i.test(options.method)) {
+      x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    }
+    x.send(options.data);
+  }
+
+  doCORSRequest({
+      method:  'GET',
+      url: 'https://scene.eldertaleonline.com/api/cards.json',
+      data: ''
+    }, function printResult(result) {
+      var data = result.responseText;
+      data = JSON.parse(data);
+      console.log(data);
+  });
