2fe5981e204e02be46cf7d6841e00ada46a31ad7
kran4 <cuiusvis.hominis.est.errare@gmail.com>
2020-08-05 22:09:04 -0400

    2 files modified

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ import angular from 'angular';
 angular.module('app', []);
 
 
+  
+
+
+// Put application code here before bootstrap is called.
+angular.module('app').controller('MainCtrl', function ($scope){
+  var s = $scope;
   var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+
+  //define a function to proxy out HTTP requests
   function doCORSRequest(options, fcn) {
     var x = new XMLHttpRequest();
     x.open(options.method, cors_api_url + options.url);
@@ -16,26 +24,21 @@ angular.module('app', []);
     }
     x.send(options.data);
   }
-  var data;
+  s.data = [];
 
+  if(!(s.message))
+    s.message = "Loading."
+  //acquire data from ETO
   doCORSRequest({
       method:  'GET',
       url: 'https://scene.eldertaleonline.com/api/cards.json',
       data: ''
-    }, function printResult(result) {
-      data = result.responseText;
-      data = JSON.parse(data);
-      console.log(data);
+    }, function(result) {
+      s.data = result.responseText;
+      s.data = JSON.parse(s.data);
+      s.message = "Load complete.";
+      console.log("Success");
   });
-
-
-// Put application code here before bootstrap is called.
-angular.module('app').controller('MainCtrl', function ($scope){
-  $scope.message = 'hello';
-
-  $scope.updateMessage = function(message){
-    $scope.message = data.length;
-  };
 });
 
 
