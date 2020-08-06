f134a4e7aeb012545c7e2fc73205d22f33cafb3d
kran4 <cuiusvis.hominis.est.errare@gmail.com>
2020-08-06 18:50:31 -0400

    2 files modified

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ import angular from 'angular';
 // Create an angular module named 'app'.
 angular.module('app', []);
 
-
-  
-
-
 // Put application code here before bootstrap is called.
 angular.module('app').controller('MainCtrl', function ($scope){
   var s = $scope;
   var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+
+  s.headers = [
+    {display: "Title", col: "Title"},
+    {display: "Type", col: "type"},
+    {display: "Rarity", col: "rarity"},
+    {display: "Description", col: "Description"},
+    {display: "Requires", col: "require"},
+    {display: "Grants", col: "grant"},
+    {display: "Targets", col: "target"}
+
+  ];
 
   //define a function to proxy out HTTP requests
   function doCORSRequest(options, fcn) {
@@ -25,6 +32,7 @@ angular.module('app').controller('MainCtrl', function ($scope){
     x.send(options.data);
   }
   s.data = [];
+  s.loadComplete = false;
 
   if(!(s.message))
     s.message = "Loading."
@@ -36,19 +44,20 @@ angular.module('app').controller('MainCtrl', function ($scope){
     }, function(result) {
       s.data = result.responseText;
       s.data = JSON.parse(s.data);
-      s.message = "Load complete.";
+      s.loadComplete = true;
+      processData(s.data);
       console.log("Success");
       console.log(s.data);
-      processData(s.data);
   });
 
   function processData(data) {
-    for(var i in data) {
-    data[i].type = data[i].Data.type;
-    data[i].require = concatenateConcepts(data[i].Data.conceptrequirement);
-    data[i].grant = concatenateConcepts(data[i].Data.concept);
-    data[i].target = data[i].Data.targets ? data[i].Data.targets.join(', ') : '';
-}
+      for(var i in data) {
+        data[i].type = data[i].Data.type;
+        data[i].require = concatenateConcepts(data[i].Data.conceptrequirement);
+        data[i].grant = concatenateConcepts(data[i].Data.concept);
+        data[i].target = data[i].Data.targets ? data[i].Data.targets.join(', ') : '';
+        data[i].rarity = data[i].Data.rarity;
+    }
   }
 
   function concatenateConcepts(arr) {
@@ -65,6 +74,18 @@ angular.module('app').controller('MainCtrl', function ($scope){
 
   s.update = function() {
     console.log(s.data.length);
+  };
+
+  s.sortBy = s.headers[0].col; //default to first column
+  s.sort = function(col) {
+    if(col != s.sortBy)
+      s.sortBy = col;
+    else
+      s.sortBy = '-' + s.sortBy;
+  }
+
+  s.filterBy = function(item) {
+    
   };
 });
 
