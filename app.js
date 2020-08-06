db3aee35efccb02818075081255f1640cbf62ab6
kran4 <cuiusvis.hominis.est.errare@gmail.com>
2020-08-05 21:58:22 -0400

    2 files modified

--- app.js
+++ app.js
@@ -3,17 +3,6 @@ import angular from 'angular';
 // Create an angular module named 'app'.
 angular.module('app', []);
 
-// Put application code here before bootstrap is called.
-angular.module('app').controller('MainCtrl', function ($scope){
-  $scope.message = 'hello';
-
-  $scope.updateMessage = function(message){
-    $scope.message = message;
-  };
-});
-
-// Bootstrap angular onto the 'app' element, injecting the 'app' module.
-angular.bootstrap(document.getElementById('app'), ['app']);
 
   var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
   function doCORSRequest(options, fcn) {
@@ -27,13 +16,28 @@ angular.bootstrap(document.getElementById('app'), ['app']);
     }
     x.send(options.data);
   }
+  var data;
 
   doCORSRequest({
       method:  'GET',
       url: 'https://scene.eldertaleonline.com/api/cards.json',
       data: ''
     }, function printResult(result) {
-      var data = result.responseText;
+      data = result.responseText;
       data = JSON.parse(data);
       console.log(data);
   });
+
+
+// Put application code here before bootstrap is called.
+angular.module('app').controller('MainCtrl', function ($scope){
+  $scope.message = 'hello';
+
+  $scope.updateMessage = function(message){
+    $scope.message = data.length;
+  };
+});
+
+
+// Bootstrap angular onto the 'app' element, injecting the 'app' module.
+angular.bootstrap(document.getElementById('app'), ['app']);
